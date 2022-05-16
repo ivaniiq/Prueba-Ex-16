bb34976c105ccab5518d209f17bf5397d1bccae4
Ivan Ibañez Quirant <gizboy@hotmail.com>
2022-05-16 21:34:40 +0200

    Colocados nuevos elementos, un textbox para indicar el tipo de saludo que se encadena al textbox del nombre.

--- PruebaEx_16/Form1.Designer.cs
+++ PruebaEx_16/Form1.Designer.cs
@@ -31,18 +31,21 @@
             this.textBox = new System.Windows.Forms.TextBox();
             this.button = new System.Windows.Forms.Button();
             this.labelSaludo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelTipSaludo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox
             // 
-            this.textBox.Location = new System.Drawing.Point(42, 41);
+            this.textBox.Location = new System.Drawing.Point(230, 58);
             this.textBox.Name = "textBox";
             this.textBox.Size = new System.Drawing.Size(358, 27);
             this.textBox.TabIndex = 0;
             // 
             // button
             // 
-            this.button.Location = new System.Drawing.Point(128, 150);
+            this.button.Location = new System.Drawing.Point(316, 194);
             this.button.Name = "button";
             this.button.Size = new System.Drawing.Size(185, 29);
             this.button.TabIndex = 1;
@@ -53,16 +56,44 @@
             // labelSaludo
             // 
             this.labelSaludo.AutoSize = true;
-            this.labelSaludo.Location = new System.Drawing.Point(200, 314);
+            this.labelSaludo.Location = new System.Drawing.Point(388, 331);
             this.labelSaludo.Name = "labelSaludo";
             this.labelSaludo.Size = new System.Drawing.Size(0, 20);
             this.labelSaludo.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(231, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(357, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(152, 61);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(64, 20);
+            this.labelNombre.TabIndex = 4;
+            this.labelNombre.Text = "Nombre";
+            // 
+            // labelTipSaludo
+            // 
+            this.labelTipSaludo.AutoSize = true;
+            this.labelTipSaludo.Location = new System.Drawing.Point(154, 125);
+            this.labelTipSaludo.Name = "labelTipSaludo";
+            this.labelTipSaludo.Size = new System.Drawing.Size(55, 20);
+            this.labelTipSaludo.TabIndex = 5;
+            this.labelTipSaludo.Text = "Saludo";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 392);
+            this.ClientSize = new System.Drawing.Size(838, 521);
+            this.Controls.Add(this.labelTipSaludo);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.labelSaludo);
             this.Controls.Add(this.button);
             this.Controls.Add(this.textBox);
@@ -78,5 +109,8 @@
         private TextBox textBox;
         private Button button;
         private Label labelSaludo;
+        private TextBox textBox1;
+        private Label labelNombre;
+        private Label labelTipSaludo;
     }
 }
